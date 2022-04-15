017dc6b5fbc930ae45572c0b6b45c3041d275bed
lowchoonkeat <91364310+lowchoonkeat@users.noreply.github.com>
2022-04-15 13:05:19 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "employee.cxdymafgd8jd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
